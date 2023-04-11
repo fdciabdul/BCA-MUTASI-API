b2ea4665f9edd32604c72d4cbce3d122be7534e5
taqin <abdulmuttaqin456@gmail.com>
2023-04-11 17:12:49 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# BCA-MUTASI-API
+REST API FOR GET MUTASI FROM BCA 
+
+✅ Auto get Mutasi
+
+✅ Cron Job / Account
+
+✅ Save Mutasi to database
+
+✅ Save credentials to database
+
+
+
+
---
+++ app/config/Database.js
@@ -0,0 +1,9 @@
+module.exports ={
+    host : '',
+    username : 'root',
+    password : '',
+    database : '',
+    headless : false, // config untuk menampilkan browser ( true = tidak tampil / false =  tampil) 
+}
+
+//GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'P@55w0rd';
